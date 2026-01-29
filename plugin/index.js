d76c7ce937b45afb3f4562e0518ad81c84124c8a
Matas Noreika <matassimkus55@gmail.com>
2026-01-29 12:09:37 +0000

    Init commit

---
+++ plugin/index.js
@@ -0,0 +1,26 @@
+/*
+ * Purpose:
+ * Central script that handles the plugin configuration.
+ * Programmer: Matas Noreika 26/01/29 11:51:23
+*/
+
+module.exports = (app) => {
+  const plugin = {
+    id: "signalk-collision-detection",
+    name: "Collision detection",
+    start (settings, restartPlugin) => {
+      //start-up code goes here
+      console.log("Recieved these settings: ", settings);
+    },
+    stop: () => {
+      //close-up code goes here
+    },
+    schema: () => {
+      properties: {
+        //plugin configuration goes here
+      }
+    }
+  }
+
+  return plugin;
+}

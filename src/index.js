aa4e29cb6e4aaf7318b59f408efa717e6dc55421
matas-noreika <matassimkus55@gmail.com>
2026-03-26 11:59:41 +0000

    Configured delta emit on PosForm submission

--- src/index.js
+++ src/index.js
@@ -17,17 +17,17 @@
 // The plugin function receives the app object as an argument, which can be used to access the Signal K server and other plugins.
 
 module.exports = (app) => {//start of module.exports function
-  
+
   // id allows the plugin to be identified and managed by the Signal K server
   // It is also the name used for API endpoints under /plugins/
   const PLUGIN_ID = "signalk-collision-detection";
   //human-readable name for the plugin, used in the UI
   const PLUGIN_NAME = "Collision detection";
-  const start = (config,restartCallback) => {//start of start()
+  const start = (config, restartCallback) => {//start of start()
     //start-up code goes here
     // do nothing for now, just log the settings to the console
     console.log("Recieved these settings: ", settings);
-    console.log("Vessel id: ",app.selfId);
+    console.log("Vessel id: ", app.selfId);
   };//end of start()
   const stop = () => {//start of stop()
 
@@ -45,32 +45,44 @@ module.exports = (app) => {//start of module.exports function
   const getPos = (req, res) => {//start of getPos()
     const nav_data = app.getSelfPath("navigation");
     app.debug("navigation data: ", nav_data);
-    try{
+    try {
       //check if navigation data exists
-      if(typeof nav_data == 'undefined'){
+      if (typeof nav_data == 'undefined') {
         throw new Error('Navigation data missing!');
       }
       //check if position is a member object of nav_data
-      if(!nav_data.hasOwnProperty('position')){
+      if (!nav_data.hasOwnProperty('position')) {
         throw new Error('Position data missing');
       }
-      if(!nav_data.position.hasOwnProperty('value')){
+      if (!nav_data.position.hasOwnProperty('value')) {
         throw new Error('value property missing in position data');
       }
       //send our position data
       app.debug("position data: ", nav_data.position.value);
       res.status(200).json(nav_data.position.value);
-    }catch(error){
+    } catch (error) {
       //Error objects don't parse like regular objects
       //JSON.stringify() returns, {} for errors
       app.debug(error);
-      res.status(200).json({message: error.message});
+      res.status(200).json({ message: error.message });
     }
   };//end of getPos()
   const setPos = (req, res) => {//start of setPos()
     app.debug(req.body);
+    //send a delta message to server
+    app.handleMessage(PLUGIN_ID, {
+      updates: [{
+        values: [{
+          path: "navigation.position",
+          value: {
+            longitude: req.body.latitude,
+            latitude: req.body.longitude
+          }
+        }]
+      }]
+    });
     //write data to signalk data model
-    res.status(200).json({message: 'position was set successfully!'});
+    res.status(200).json({ message: 'position was set successfully!' });
   };//end of setPos()
   const plugin = {//start of plugin object
     id: PLUGIN_ID,
